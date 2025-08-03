6260b13d9bf2c6fb6f63261147f32628f5ab1967
Juma Rubea <rubeajuma8@gmail.com>
2025-08-03 15:10:53 +0300

    feat: tool calling

--- wcfbot/back-end/main.py
+++ wcfbot/back-end/main.py
@@ -38,5 +38,5 @@ app.include_router(audio_router, prefix="/v1/audio")
 
 if __name__ == "__main__":
     app_host = os.getenv("APP_HOST", "0.0.0.0")
-    app_port = int(os.getenv("APP_PORT", "8001"))
+    app_port = int(os.getenv("APP_PORT", "8088"))
     uvicorn.run(app="main:app", host=app_host, port=app_port, reload=True)
